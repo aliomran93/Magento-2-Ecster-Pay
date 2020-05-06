acf9c9d1f73cf2edb668f6b697d14453352c57db
AndreKlang <andre.klang@evalent.com>
2020-05-06 09:12:35 +0200

    OEN Support for 2.2 & 2.3
    Magento 2.2 & 2.3 have incompatible requirements for controllers handling POST requests.
    This adds two separate url's, used depending on version.
    
    Also renamed controller from "opn" to "oen", since Ecster refer to this as "Order Event Notification".

--- src/Model/Api/Ecster.php
+++ src/Model/Api/Ecster.php
@@ -237,7 +237,12 @@ class Ecster
 
     public function getNotificationUrl()
     {
-        return $this->_urlBuilder->getUrl('ecsterpay/checkout/opn', ['_secure' => true]);
+        // if CsrfAwareActionInterface exists (magento is >=2.3)
+        if (interface_exists('\Magento\Framework\App\CsrfAwareActionInterface')) {
+            return $this->_urlBuilder->getUrl('ecsterpay/checkout/oenV2', ['_secure' => true]);
+        }
+
+        return $this->_urlBuilder->getUrl('ecsterpay/checkout/oenV1', ['_secure' => true]);
     }
 
     protected function getApiKey()
